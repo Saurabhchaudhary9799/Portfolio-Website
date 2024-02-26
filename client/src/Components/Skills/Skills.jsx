754411f0c8299e5b434b321495e8817b69bb1a84
Saurabh_Chaudhary <saurabh@localhost.localdomain>
2024-02-26 19:33:59 +0530

    third commit

--- client/src/Components/Skills/Skills.jsx
+++ client/src/Components/Skills/Skills.jsx
@@ -1,73 +1,79 @@
+import { Box, Center, Text, Wrap, WrapItem } from "@chakra-ui/react";
 import React from "react";
 import { motion } from "framer-motion";
-import "./Skills.css";
-import {
-  Box,
-  Center,
-  CircularProgress,
-  CircularProgressLabel,
-  Progress,
-  Text,
-  Wrap,
-  WrapItem,
-} from "@chakra-ui/react";
+
+const fadeInAnimationVariants = {
+  initial: {
+    opacity: 0,
+    y: 100,
+  },
+  animate: (index) => ({
+    opacity: 1,
+    y: 0,
+    transiton: { delay: 0.05 * index },
+  }),
+};
+
 const Skills = () => {
   const skills = [
-    { name: "HTML", value: 80 },
-    { name: "CSS", value: 80 },
-    { name: "Javascript", value: 80 },
-    { name: "Node JS", value: 80 },
-    { name: "Express Js", value: 60 },
-    { name: "MongoDB", value: 80 },
-    { name: "Bootstrap", value: 80 },
-    { name: "React JS", value: 80 },
-    { name: "Redux", value: 80 },
-    { name: "Operating System", value: 80 },
-    { name: "Computer Network", value: 80 },
-    { name: "C++", value: 80 },
-    { name: "OOPs", value: 80 },
+    "HTML",
+    "CSS",
+    "Javascript",
+    "Bootstrap",
+    "React JS",
+    "Redux",
+    "NodeJS",
+    "ExpressJS",
+    "MongoDB",
+    "Computer Network",
+    "Operating System",
+    "REST api",
+    "mongoose",
+    "c++",
+    "Chakra UI",
+    "Framer Motion",
   ];
   return (
-    <>
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          flexDirection: "column",
-          margin: "10px 100px",
-        }}
-      >
-        <Text
-          as={motion.div}
-          initial={{ x: -500 }}
-          animate={{ x: 0 }}
-          fontSize={"40px"}
-          color={"orange"}
-          fontWeight={"700"}
-        >
+    <Box
+      sx={{
+        margin: "10px 100px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        gap: "30px",
+      }}
+    >
+      <Box>
+        <Text sx={{ fontSize: "40px", color: "orange", fontWeight: "700" }}>
           Skills
         </Text>
-        <Box
-          sx={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}
-        >
-          {skills.map((skill, i) => {
-            return (
-              <Box className="container" key={i}>
-                <Box className="skill-box">
-                  <Box className="skill-bar">
-                    <span className="skill-per html">
-                      <span className="tooltip">{skill.value}%</span>
-                    </span>
-                  </Box>
-                  <h3>{skill.name}</h3>
-                </Box>
-              </Box>
-            );
-          })}
-        </Box>
       </Box>
-    </>
+      <Wrap>
+        {skills.map((skill, index) => {
+          return (
+            <WrapItem
+              key={index}
+              as={motion.div}
+              variants={fadeInAnimationVariants}
+              initial="initial"
+              whileInView="animate"
+              viewport={{ once: true }}
+              custom={index}
+              sx={{
+                border: "1px solid white",
+                padding: "5px",
+                boxShadow:
+                  "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+                margin: "3px 5px",
+              }}
+            >
+              <Center bg="red.200">{skill}</Center>
+            </WrapItem>
+          );
+        })}{" "}
+      </Wrap>
+    </Box>
   );
 };
 
